6daf1e28c2adfc83e665d74a20856e0acd6c5de3
Reuben deVries <reubendevries@gmail.com>
2021-02-14 22:35:06 -0800

    Add files via upload

---
+++ sliceback.py
@@ -0,0 +1,15 @@
+#          01234567890123456789012345
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+a = letters[16:13:-1]
+print(a)
+
+print()
+
+b = letters[4::-1]
+print(b)
+
+print()
+
+c = letters[:17:-1]
+print(c)
---
+++ strings.py
@@ -0,0 +1,20 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name: ")
+print(greeting + name)
+
+#if we want to add a space, we can add that too...
+
+print(greeting + " " + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+print(name + f" is {age} years old")
